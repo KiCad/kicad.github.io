ab9bb8278dd9568aa2b6291cd8424a22bfc3721f
Oliver <oliver.henry.walters@gmail.com>
2017-11-02 01:12:11 +1100

    Hundreth time is the charm

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd $TRAVIS_BUILD_DIR
 rm ./_symbols/*.md
 rm ./_footprints/*.md
 rm ./_packages3d/*.md
+rm ./download/*
 
 # Run generator scripts
 cd ./_scripts
@@ -17,10 +18,13 @@ SHA=`git rev-parse --verify HEAD`
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-git add -A .
+git add _symbols
+git add _footprints
+git add _packages3d
+git add download
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
+if git diff origin/master--quiet; then
     echo "No changes found; exiting."
     exit 0
 fi
