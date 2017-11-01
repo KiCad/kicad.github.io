59b16e4735a93c81a4cc107faeb103d226e16f43
Oliver <oliver.henry.walters@gmail.com>
2017-11-01 23:20:31 +1100

    More debugging needed

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ rm ./_packages3d/*.md
 cd ./_scripts
 echo "what is here?"
 ls .
+echo "What is there?"
+ls /home/travis/build/kicad-library/
+echo "What is in libs?"
+ls /home/travis/build/kicad-library/library/
 python gen_symbol_info.py /home/travis/kicad-library/library/*.lib --schlib /home/travis/build/utils/schlib --output $TRAVIS_BUILD_DIR/_symbols --download $TRAVIS_BUILD_DIR/download/ --csv $TRAVIS_BUILD_DIR/_data/symbols.csv -v
 
 echo "Ok, script is done now"
