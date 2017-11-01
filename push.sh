2b47d08a9fc3a8df508ae072ce4d01df7fd10404
Oliver <oliver.henry.walters@gmail.com>
2017-11-02 01:49:10 +1100

    And again?

---
+++ push.sh
@@ -0,0 +1,27 @@
+cd $TRAVIS_BUILD_DIR
+
+SHA=`git rev-parse --verify HEAD`
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+git add _symbols
+git add _footprints
+git add _packages3d
+git add download
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff origin/master --quiet; then
+    echo "No changes found; exiting."
+    exit 0
+fi
+
+git commit -m "Autobuild by Travis: ${SHA}"
+
+chmod 600 .id_rsa
+eval `ssh-agent -s`
+
+ssh-add .id_rsa
+
+# And push back upstream!
+git push https://github.com/KiCad/kicad.github.io master
