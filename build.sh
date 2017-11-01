7eb15cd4fe9c4c991dd67cf287a3653bb583d397
Oliver <oliver.henry.walters@gmail.com>
2017-11-02 08:58:37 +1100

    Trying just a single .lib file
    Yaaaay

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm ./download/*
 
 # Run generator scripts
 cd ./_scripts
-python gen_symbol_info.py /home/travis/build/kicad-library/library/*.lib --schlib /home/travis/build/utils/schlib --output $TRAVIS_BUILD_DIR/_symbols --download $TRAVIS_BUILD_DIR/download/ --csv $TRAVIS_BUILD_DIR/_data/symbols.csv -v
+python gen_symbol_info.py /home/travis/build/kicad-library/library/LED.lib --schlib /home/travis/build/utils/schlib --output $TRAVIS_BUILD_DIR/_symbols --download $TRAVIS_BUILD_DIR/download/ --csv $TRAVIS_BUILD_DIR/_data/symbols.csv -v
 
 # And back to the build dir
 cd $TRAVIS_BUILD_DIR
