669355ee27433c7e8ff93e5ba5d689e66e669739
Oliver <oliver.henry.walters@gmail.com>
2017-11-03 10:01:48 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
 # Remove old generated data
 cd $TRAVIS_BUILD_DIR
 
