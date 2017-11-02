92265bc16f1df6bca064fad0a2c5e8d92fb74f5b
Oliver <oliver.henry.walters@gmail.com>
2017-11-03 08:35:39 +1100

    Push to gh-pages branch

--- push.sh
+++ push.sh
@@ -28,4 +28,4 @@ ssh-add travis_key
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 # And push back upstream (must use SSH repo URL)
-git push git@github.com:KiCad/kicad.github.io.git master -f
+git push git@github.com:KiCad/kicad.github.io.git gh-pages -f
