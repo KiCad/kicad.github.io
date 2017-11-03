978e871de079df45a8de6ae0f3b8f26525670967
Oliver <oliver.henry.walters@gmail.com>
2017-11-03 22:47:02 +1100

    Updates to archive script
    - Make temp copy first
    - Only copy temp file to output dir if change detected (md5sum)
    - Limits git diff noise

--- _scripts/zipper.py
+++ _scripts/zipper.py
@@ -5,18 +5,51 @@ Functions for compressing files for distribution
 import os
 from subprocess import call, Popen, PIPE
 
-def archive_7z(archive, files, **kwargs):
+def file_md5(file):
+    fn = os.path.abspath(file)
+
+    if os.path.exists(file):
+        p = Popen(['md5sum', file], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate()
+
+        return output.split()[0]
+
+    else:
+        return None
+
+def archive_7z(archive, files):
+
+    # Archive to a temp location first
+    # Only copy to output path if it is different
+    tmp_file = "/tmp/archive.7z"
+
+    # Delete temp file
+    if os.path.exists(tmp_file):
+        call(['rm', tmp_file])
 
     # Add each file to the archive individually
     for f in files:
-        args = ['7z', 'a', '-t7z', '-mx=9', os.path.abspath(archive), f]
+        args = ['7z', 'a', '-t7z', '-mx=9', os.path.abspath(tmp_file), f]
         call(args)
 
-
-    p = Popen(['ls', '-lh', archive], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p = Popen(['ls', '-lh', tmp_file], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
     output, err = p.communicate()
 
+    # Calculate archive size
     sz = output.split(' ')[4]
+
+    copy = True
+
+    if os.path.exists(archive):
+        md5_archive = file_md5(archive)
+        md5_tmp = file_md5(tmp_file)
+
+        if md5_tmp == md5_archive:
+            print("No changed to archive {f} - skipping".format(f=os.path.basename(archive)))
+            copy = False
+
+    if copy:
+        call(['mv', tmp_file, archive])
 
     return str(sz)
