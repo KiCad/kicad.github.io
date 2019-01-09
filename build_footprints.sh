4ddc76b29df75489c2841cc3640e800493d0384c
Grey Christoforo <grey@christoforo.net>
2019-01-09 14:50:30 +0000

    copy over fp-lib-table to downloads directory

--- build_footprints.sh
+++ build_footprints.sh
@@ -10,5 +10,8 @@ cd $TRAVIS_BUILD_DIR
 rm _footprints/*.md
 rm _footprints/*.html
 
+# copy over footprint library table to downloads directory
+cp /home/travis/build/footprints/fp-lib-table $TRAVIS_BUILD_DIR/download/footprints/.
+
 # Generate footprint data
 python _scripts/gen_footprint_info.py /home/travis/build/footprints/*.pretty --script /home/travis/build/utils -v --download $TRAVIS_BUILD_DIR/download --output $TRAVIS_BUILD_DIR/_footprints
